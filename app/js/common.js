5c07f6bfddc7abee2934968687e53ae30e63509b
Viktoria <zaharova181114@gmail.com>
2020-12-23 18:11:20 +0200

    page +article

--- app/js/common.js
+++ app/js/common.js
@@ -46,6 +46,24 @@ $('.gallery-clients-slider').slick({
     ]
 });
 
+$('.article-gallery__slider').slick({
+    slidesToShow: 2,
+    prevArrow: '<button type="button" class="slick-prev"><svg width="16" height="29" viewBox="0 0 16 29" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M14.5 1L1 14.5L14.5 28" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n</button>',
+    nextArrow: '<button type="button" class="slick-next"><svg width="16" height="29" viewBox="0 0 16 29" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '<path d="M1.5 28L15 14.5L1.5 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n</button>',
+    responsive: [
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
 // mobile menu
 $('.btn-close').click(function (){
    $('.mobile-menu').fadeOut();
