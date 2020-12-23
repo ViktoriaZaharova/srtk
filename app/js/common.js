df1a07a0a577fd671509aec35790c5e7cced726b
Viktoria <zaharova181114@gmail.com>
2020-12-23 08:53:14 +0200

    media

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,27 @@ $(".news-slider").slick({
         '</svg>\n</button>',
     nextArrow: '<button type="button" class="slick-next"><svg width="16" height="29" viewBox="0 0 16 29" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
         '<path d="M1.5 28L15 14.5L1.5 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-        '</svg>\n</button>'
+        '</svg>\n</button>',
+    responsive: [
+        {
+            breakpoint: 1070,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 850,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 580,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 $('.gallery-clients-slider').slick({
@@ -15,5 +35,22 @@ $('.gallery-clients-slider').slick({
         '</svg>\n</button>',
     nextArrow: '<button type="button" class="slick-next"><svg width="16" height="29" viewBox="0 0 16 29" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
         '<path d="M1.5 28L15 14.5L1.5 1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-        '</svg>\n</button>'
+        '</svg>\n</button>',
+    responsive: [
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+// mobile menu
+$('.btn-close').click(function (){
+   $('.mobile-menu').fadeOut();
+});
+
+$('.btn-burger').click(function (){
+    $('.mobile-menu').fadeToggle();
 });
